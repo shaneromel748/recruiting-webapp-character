2022f1345649362728d091f712df4ec1d4e22169
Shane Romel Kujur <shaneromel@Shanes-MacBook-Pro.local>
2024-10-05 15:43:50 -0400

    implemented character class

--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -1,11 +1,34 @@
-import { Card } from "antd";
-import { useState } from "react";
+import { Button, Card } from "antd";
+import { useEffect, useState } from "react";
 import Attribute from "./Attribute";
+import { CLASS_LIST } from "../consts";
 
 export default function Character({
     character
 }) {
     const [attributes, setAttributes] = useState(character.attributes);
+    const [selectedClass, setSelectedClass] = useState();
+    const [eligibleClasses, setEligibleClasses] = useState([]);
+
+    useEffect(() => {
+        for (let classTitle in CLASS_LIST) {
+            const requirements = CLASS_LIST[classTitle];
+            let isEligible = true;
+
+            for (let requirement in requirements) {
+                const requirementValue = requirements[requirement];
+                const characterValue = attributes[requirement].points || 0;
+
+                if(requirementValue > characterValue) {
+                    isEligible = false;
+                }
+            }
+
+            if(isEligible) {
+                setEligibleClasses(prevEligibleClasses => [...prevEligibleClasses, classTitle]);
+            }
+        }
+    }, [attributes]);
 
     const handleAttributesChange = (attributeTitle, newPoints) => {
         setAttributes(prevAttributes => {
@@ -44,6 +67,33 @@ export default function Character({
                     ))}
                 </div>
             </Card>
+
+            <Card title="Classes">
+                <div className="flex flex-col items-center">
+                    {Object.keys(CLASS_LIST).map((classTitle, index) => (
+                        <span key={index} onClick={() => setSelectedClass({
+                            title: classTitle,
+                            requirements: CLASS_LIST[classTitle]
+                        })} className="cursor-pointer" style={{
+                            color: eligibleClasses.includes(classTitle) ? "green" : "black"
+                        }}>{classTitle}</span>
+                    ))}
+                </div>
+            </Card>
+
+            {selectedClass && (
+                <Card 
+                    actions={[<Button onClick={() => setSelectedClass()}>Close Requirements</Button>]}
+                    title={<>{selectedClass.title} minimum requirements</>}
+                >
+                    {Object.keys(selectedClass.requirements).map((requirement, index) => (
+                        <div key={index} className="flex gap-2 justify-between">
+                            <span>{requirement}</span>
+                            <span>{selectedClass.requirements[requirement]}</span>
+                        </div>
+                    ))}
+                </Card>
+            )}
         </div>
     )
 }
