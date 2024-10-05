885edd8011a18c940d84916718918fe1452d4c68
Shane Romel Kujur <shaneromel@Shanes-MacBook-Pro.local>
2024-10-05 15:13:05 -0400

    fetch character API implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import { useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import './App.css';
 import { ATTRIBUTE_LIST, CLASS_LIST, SKILL_LIST } from './consts.js';
+import { notification } from 'antd';
+import { getCharacters } from './utils.js';
 
 
 function App() {
-  const [num, setNum] = useState(0);
+  const [notificationApi, notificationContextHolder] = notification.useNotification();
+  const [characters, setCharacters] = useState([]);
+
+  const fetchCharacters = useCallback(async () => {
+    try {
+      const characters = await getCharacters();
+      console.log("characters", characters);
+      setCharacters(characters);
+    } catch (err) {
+      console.log(err);
+      notificationApi.error({
+        message: "Something went wrong while fetching characters",
+        type: "error"
+      });
+    }
+  }, [notificationApi]);
+
+  useEffect(() => {
+    fetchCharacters();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>React Coding Exercise</h1>
       </header>
       <section className="App-section">
-        <div>
-          Value:
-          {num}
-          <button>+</button>
-          <button>-</button>
-        </div>
+
       </section>
+      {notificationContextHolder}
     </div>
   );
 }
---
+++ src/utils.js
@@ -0,0 +1,7 @@
+import { default as axios} from 'axios';
+
+const characterApi = axios.create({
+    baseURL: "https://recruiting.verylongdomaintotestwith.ca/api/{shaneromel748}"
+});
+
+export const getCharacters = () => characterApi.get("/character").then(response => response.data?.body);
