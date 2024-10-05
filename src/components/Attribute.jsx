3bad02baf5ef6e2fd8a0575c752a23386e0e251f
Shane Romel Kujur <shaneromel@Shanes-MacBook-Pro.local>
2024-10-05 15:33:49 -0400

    attribute update logic added

--- src/components/Attribute.jsx
+++ src/components/Attribute.jsx
@@ -1,24 +1,29 @@
-import { useEffect, useState } from "react"
+import { useCallback, useEffect, useState } from "react"
 import { getModifierValue } from "../utils";
 import { Button } from "antd";
 
 export default function Attribute({
-    attribute
+    attribute,
+    onPointsChange
 }) {
     const [modifier, setModifier] = useState(0);
 
     useEffect(() => {
         const modifier = getModifierValue(attribute.points);
         setModifier(modifier); 
-    }, []);
+    }, [attribute]);
+
+    const updatePoints = useCallback((value) => {
+        onPointsChange(attribute.title, attribute.points + value);
+    }, [attribute]);
 
     return (
         <div className="flex gap-4 items-center justify-between">
             <span>{attribute.title}: {attribute.points} (Modifier: {modifier})</span>
 
             <div className="flex gap-1">
-                <Button>-</Button>
-                <Button>+</Button>
+                <Button onClick={() => updatePoints(-1)}>-</Button>
+                <Button onClick={() => updatePoints(1)}>+</Button>
             </div>
         </div>
     )
--- src/components/Character.jsx
+++ src/components/Character.jsx
@@ -5,19 +5,41 @@ import Attribute from "./Attribute";
 export default function Character({
     character
 }) {
-    const [attributes, setAttrbutes] = useState(character.attributes);
+    const [attributes, setAttributes] = useState(character.attributes);
+
+    const handleAttributesChange = (attributeTitle, newPoints) => {
+        setAttributes(prevAttributes => {
+            const updatedAttributes = {
+                ...prevAttributes,
+                [attributeTitle]: {
+                    ...prevAttributes[attributeTitle],
+                    points: newPoints
+                }
+            };
+
+            const totalAttributePoints = Object.keys(updatedAttributes).reduce((acc, curr) => acc + updatedAttributes[curr].points, 0);
+
+            if (totalAttributePoints <= 70) {
+                return updatedAttributes;
+            } else {
+                alert("Maximum attribute points reached");
+                return prevAttributes;
+            }
+        })
+    }
 
     return (
         <div className="flex gap-3">
             <Card title="Attributes">
                 <div className="flex flex-col">
                     {Object.keys(attributes).map((attributeTitle, index) => (
-                        <Attribute 
+                        <Attribute
                             key={index}
                             attribute={{
                                 title: attributeTitle,
                                 points: attributes[attributeTitle].points
                             }}
+                            onPointsChange={handleAttributesChange}
                         />
                     ))}
                 </div>
